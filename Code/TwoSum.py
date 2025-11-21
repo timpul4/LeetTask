9fbd3c17d2e66cd3248c203444ec6b8ccc40c763
Pulatov Timur <150252978+timpul4@users.noreply.github.com>
2025-11-21 16:18:07 +0300

    Add files via upload

---
+++ Code/TwoSum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            need = target - num
+            if need in seen:
+                return [seen[need], i]
+            seen[num] = i
+        return []
